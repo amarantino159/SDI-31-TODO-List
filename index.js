f40ee58edb65c5848d01390f5c0de16bce2990fe
amarantino159 <amarantino159@gmail.com>
2025-07-09 09:54:40 -0700

    add up to js dom items

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('My code running');
+let btn= document.querySelector('#submit');
+// console.log(document.body);
+// let btn = document.getElementById("submit");
+console.log(btn);
+let newItem=document.querySelector('#newItem');
+console.log(newItem);
+btn.addEventListener('click', ()=>alert('pressed'));
